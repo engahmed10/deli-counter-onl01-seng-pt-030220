9778a402f4f0b94197d73bafe876ded796ac1658
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-05 19:36:10 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,46 @@
 # Write your code here.
+
+def line(array)
+
+
+str =  "The line is currently:"
+str1 =""
+  if array.length == 0
+      return  puts  "The line is currently empty."
+  elsif array.length < 4
+
+    n=1
+    array.each do |i|
+    str1 =str1 + " #{n}. #{i}"
+    n +=1
+    end
+  else
+      n=1
+    array.each do |i|
+      str1 =str1 + " #{n}. #{i}"
+      n +=1
+    end
+  end
+    puts "#{str}#{str1}"
+end
+
+def take_a_number(array,name)
+
+    if array.length == 0
+      array.push(name)
+      puts  "Welcome, #{name}. You are number 1 in line."
+    else
+      array.push(name)
+    puts  "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+    end
+
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+     puts "Currently serving #{array[0]}."
+     array.shift
+  end
+end
